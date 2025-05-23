4e38aed093211e9affdd78229502d5e1a59c559b
Huy Tran <baohuy82.th@gmail.com>
2025-05-22 23:47:32 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import dns from 'dns'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import dns from "dns"
+import tsconfigPaths from "vite-tsconfig-paths"
+
 // https://vitejs.dev/config/server-options.html#server-options
-dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder("verbatim")
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +13,3 @@ export default defineConfig({
     port: 3000,
   },
 })
-
