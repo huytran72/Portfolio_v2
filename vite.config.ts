4ee0020edb2f6ab7011cf533324064dbcdc4220d
Huy Tran <baohuy82.th@gmail.com>
2025-05-22 23:47:49 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ dns.setDefaultResultOrder("verbatim")
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
   },
