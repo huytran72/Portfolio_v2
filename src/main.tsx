601a9b5af93658bc3ac840c0244d11d0cbd8040c
Huy Tran <baohuy82.th@gmail.com>
2025-05-27 23:11:31 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import Layout from "@/layout"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import HomePage from "./pages/home"
 import ProjectPage from "pages/project"
-import AboutPage from "pages/about"
+import AboutPage from "@/pages/about"
 import { AppContextProvider } from "./components/context/app.context"
 import "@/i18n"
 
