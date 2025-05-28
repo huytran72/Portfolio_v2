7b939f4fde7505ff0d57d40d57a76f9b51c928b3
Huy Tran <baohuy82.th@gmail.com>
2025-05-27 23:14:58 -0700

    Update app.context.tsx

--- src/components/context/app.context.tsx
+++ src/components/context/app.context.tsx
@@ -2,7 +2,11 @@ import { createContext, useContext, useEffect, useState } from "react"
 
 const AppContext = createContext(null)
 
-export const AppContextProvider = (props) => {
+export const AppContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
   const [theme, setTheme] = useState(() => {
     const initialTheme = localStorage.getItem("theme") || "light"
     return initialTheme
