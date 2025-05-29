4a1ca8a6479e2b4d3cab3cfc9e03c215ee5b247c
Huy Tran <baohuy82.th@gmail.com>
2025-05-29 08:30:57 -0700

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -57,7 +57,7 @@ const HomePage = () => {
         </Container>
       </section>
       <Divider />
-      <section>
+      <section ref={expRef}>
         <Container>
           <Experience />
         </Container>
