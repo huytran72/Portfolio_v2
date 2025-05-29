7445b930d338e1d652d24c4fccbfec55bde49b20
Huy Tran <baohuy82.th@gmail.com>
2025-05-29 08:09:09 -0700

    Update app.header.tsx

--- src/components/layout/app.header.tsx
+++ src/components/layout/app.header.tsx
@@ -7,7 +7,7 @@ import { useCurrentApp } from "../context/app.context"
 import { useTranslation } from "react-i18next"
 import { NavDropdown } from "react-bootstrap"
 import viFlag from "assets/svg/language/vi.svg"
-import enFlag from "../../assets/svg/language/en.svg"
+import enFlag from "assets/svg/language/en.svg"
 
 type ThemeContextType = "light" | "dark"
 
