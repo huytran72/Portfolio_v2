e6961655dee128bc88b928a2c89cf46a46db58ab
Huy Tran <baohuy82.th@gmail.com>
2025-05-30 23:39:57 -0700

    Update data.ts

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,5 +1,5 @@
 import nestImg from "assets/project/nestjs.png"
-import nextjsImg from "../assets/project/nextjs.jpeg"
+import nextjsImg from "assets/project/nextjs.jpeg"
 import reactNativeImg from "../assets/project/react-native.png"
 import reactUltimateImg from "../assets/project/react-ultimate.png"
 import springMvcImg from "../assets/project/spring-mvc.png"
