e80090271dc87f3a5a943f504f323165472212e7
Huy Tran <baohuy82.th@gmail.com>
2025-05-30 23:43:30 -0700

    Update data.ts

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -64,8 +64,8 @@ export const EXPERIENCES = [
 export const PROJECTS = [
   {
     id: 1,
-    imgPath: reactNativeImg,
-    title: "Taskly-App",
+    imgPath: reactUltimateImg,
+    title: "Taskly Application",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     githubLink: "https://github.com/huytran72/Taskly-App",
