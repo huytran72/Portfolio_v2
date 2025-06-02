a69732855cdc2ae198cdb1a6b3e6dee8ba718e61
Huy Tran <baohuy82.th@gmail.com>
2025-06-01 23:15:45 -0700

    Update index.tsx

--- src/components/sections/about/index.tsx
+++ src/components/sections/about/index.tsx
@@ -1,6 +1,6 @@
 import { Col, Row } from "react-bootstrap"
 import AnimationLottie from "components/share/animation-lottie"
-import codingJSON from "../../../assets/lottie/coding.json"
+import codingJSON from "assets/lottie/coding.json"
 import { DEVELOPMENT_LOTTIE } from "../../../assets/lottie/string/development"
 import { CONTACT_LOTTIE } from "../../../assets/lottie/string/contact"
 
