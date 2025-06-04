9f92321f5012b97d0d36c66bfa02fc6bb89437b1
Huy Tran <baohuy82.th@gmail.com>
2025-06-04 14:53:58 -0700

    Update hero.right.tsx

--- src/components/sections/hero/hero.right.tsx
+++ src/components/sections/hero/hero.right.tsx
@@ -20,7 +20,7 @@ const HeroRight = () => {
           <div>
             <span className="ms-4 me-2">name:</span>
             <span className="text-yellow-500">
-              {`'`}Kenny Tran{`',`}
+              {`'`}Huy Tran{`',`}
             </span>
           </div>
           <div className="ms-4 me-2 text-pink-200">
